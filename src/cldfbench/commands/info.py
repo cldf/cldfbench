7f9cd78802a398f6d2964b3efdb8261f1822c7f1
xrotwang <xrotwang@googlemail.com>
2020-03-07 15:50:49 +0100

    doc

--- src/cldfbench/commands/info.py
+++ src/cldfbench/commands/info.py
@@ -6,7 +6,11 @@ from cldfbench.cli_util import with_datasets, add_dataset_spec
 
 def register(parser):
     add_dataset_spec(parser, multiple=True)
-    parser.add_argument('--cldf', action='store_true', default=False)
+    parser.add_argument(
+        '--cldf',
+        help="Print all CLDF metadata file paths curated by a dataset.",
+        action='store_true',
+        default=False)
 
 
 def run(args):
