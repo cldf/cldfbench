8db631105f14110942ee57dc928b7f6fa5842cdd
xrotwang <xrotwang@googlemail.com>
2020-03-27 08:31:00 +0100

    release 1.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cldfbench',
-    version='1.2.2.dev0',
+    version='1.2.2',
     author='Robert Forkel',
     author_email='forkel@shh.mpg.de',
     description='Python library implementing a CLDF workbench',
