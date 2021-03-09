7b670f225c4995ceae9a9c101dfbd43f9d4f0550
Robert Forkel <xrotwang@googlemail.com>
2021-03-09 14:29:27 +0100

    bump version for development

--- src/cldfbench/__init__.py
+++ src/cldfbench/__init__.py
@@ -4,4 +4,4 @@ from cldfbench.cldf import *  # noqa: F401, F403
 from cldfbench.metadata import *  # noqa: F401, F403
 from cldfbench.scaffold import *  # noqa: F401, F403
 
-__version__ = '1.4.1'
+__version__ = '1.4.2.dev0'
