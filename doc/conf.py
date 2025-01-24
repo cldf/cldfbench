24e2f0c9111843b07db3cfef99b60d4b94cdcd07
Robert Forkel <xrotwang@googlemail.com>
2025-01-24 14:47:10 +0100

    release 1.14.1

--- doc/conf.py
+++ doc/conf.py
@@ -18,11 +18,11 @@
 # -- Project information -----------------------------------------------------
 
 project = 'cldfbench'
-copyright = '2023, Robert Forkel'
+copyright = '2025, Robert Forkel'
 author = 'Robert Forkel'
 
-release = '1.14.1.dev0'
-version = '1.14.1.dev0'
+release = '1.14.1'
+version = '1.14.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -55,7 +55,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 typehints_fully_qualified = True
 always_document_param_types = True
